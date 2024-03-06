55198ed7d8303687a9ca3ad0f176c1aa83451d20
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-03-06 13:12:54 +0100

    menu burger

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../UI/Button/Button";
 import styles from "./Nav.module.scss";
 import { Link } from "react-router-dom";
@@ -7,6 +8,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
+	const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuVisible(!isMenuVisible);
+	};
 	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 	const user = useSelector((state) => state.auth.user);
 	return (
@@ -17,6 +23,17 @@ function Nav() {
 					<p className={styles.nav__title}>Food Order App</p>
 				</div>
 			</Link>
+
+			<ul
+				className={`${styles.nav__list} ${
+					isMenuVisible ? styles.showMenu : ""
+				}`}
+			>
+				<li>O nas</li>
+				<li>Menu</li>
+				<li>Kontakt</li>
+			</ul>
+
 			<div className={styles.btns}>
 				{isAuthenticated === true ? (
 					<>
@@ -35,6 +52,9 @@ function Nav() {
 					// </Link>
 					// {/* </> */}
 				)}
+			</div>
+			<div className={styles.nav__burger}>
+				<Button onClick={toggleMenu}>Burger</Button>
 			</div>
 		</nav>
 	);
